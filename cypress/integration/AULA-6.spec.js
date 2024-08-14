56a66e2889ced590a9805dba74611b2878ed7a18
Eslana Regina <reginaeslana2011@gmail.com>
2024-08-14 09:49:21 -0300

    Aulas 6

--- cypress/integration/AULA-6.spec.js
+++ cypress/integration/AULA-6.spec.js
@@ -4,6 +4,26 @@ describe('Selecionando opções em campos de seleção suspensa',()=>{
     beforeEach(()=> cy.visit('./src/index.html')) 
 
     it('seleciona um arquivo da pasta fixtures',()=>{
+        cy.get('input[type="file"]#file-upload')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json')
+            .should(function(input){
+                expect(input[0].files[0].name).to.equal('example.json')
+            })
+    })
 
+    it('seleciona um arquivo simulando um drag-and-drop',()=>{
+        cy.get('input[type="file"]#file-upload')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+            .should(function(input){
+                expect(input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias',()=>{
+        cy.fixture('example').as('sampleFile')
+        cy.get('input[type="file"]#file-upload')
+            .selectFile('@sampleFile')
     })
 })
