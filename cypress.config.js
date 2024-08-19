bd77bb00abe1fd95e2ea71cf7b1cbbe9538354a4
Eslana Regina <reginaeslana2011@gmail.com>
2024-08-19 12:29:30 -0300

    config e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    pluginsFile: false,
-    viewportHeight: 880,
-    viewportWidth: 1280,
-})
+  viewportHeight: 880,
+  viewportWidth: 1280,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
